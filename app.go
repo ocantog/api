a25aed52656acd551716376a77a68cf5c7419ea3
Gustavo <gustavoocanto@gmail.com>
2020-07-08 12:05:22 +0800

    better structure

---
+++ app.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ocantog/api/src/support"
+)
+
+func main() {
+	// var age uint8 = 35
+
+	fmt.Println(support.Reverse("!oG ,olleH \n"))
+
+	fmt.Println(support.Testing())
+}
